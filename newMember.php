81164487c867fac722e79e802064c9b2bfb1daca
JackONeill03 <B00155573@mytudublin.ie>
2024-10-09 12:43:07 +0100

    Added Part 3

---
+++ newMember.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+</head>
+<body>
+<form action="Part2.php" method="POST">
+        Enter your firstname <input type="text"
+        name="firstname"><BR>
+        Enter your lastname <input type="text"
+        name="lastname"><BR>
+         <input type="submit">   
+
+</form>
+</body>
+</html>
